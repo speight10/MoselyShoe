9dc6b4eaa6f5b31a19b3f3f308d01ba38a23ef28
Bryan <me>
2016-07-05 08:40:41 -0500

    adding more files

--- src/main/java/com/Mosely/shoe/controller/ShoeController.java
+++ src/main/java/com/Mosely/shoe/controller/ShoeController.java
@@ -3,8 +3,11 @@ package com.Mosely.shoe.controller;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.Mosely.shoe.controller.model.Contact;
 
 @Controller
 @ComponentScan
@@ -15,6 +18,16 @@ public class ShoeController {
 	
 	 @RequestMapping(value="/", method=RequestMethod.GET)
 	    public String greetingForm(Model model) {
+		 model.addAttribute("contact",new Contact());
+	        return "index";
+	    }
+	
+	 
+	 
+	 
+	 @RequestMapping(value="/", method=RequestMethod.POST)
+	    public String greetingFormSubmit(@ModelAttribute Contact contact, Model model) {
+		 model.addAttribute("contact",contact);
 	        
 	        return "index";
 	    }
---
+++ src/main/java/com/Mosely/shoe/controller/model/Contact.java
@@ -0,0 +1,48 @@
+package com.Mosely.shoe.controller.model;
+
+public class Contact {
+	
+	
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+	private String message;
+	private String email;
+	
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+
+}
